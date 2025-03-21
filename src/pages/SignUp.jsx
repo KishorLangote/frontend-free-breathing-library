08cb6f46b5df9772fdfab10fa6f8a747effa8463
Kishor Langote <kishorlangoet2@gmail.com>
2025-03-21 17:53:37 +0530

    Updates

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import axios from "axios";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
