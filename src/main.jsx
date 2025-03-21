6b2a9ccbfa956c8c7240efe120afdfb1c0bed340
Kishor Langote <kishorlangoet2@gmail.com>
2025-03-21 17:55:02 +0530

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./components/store/index.js";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
